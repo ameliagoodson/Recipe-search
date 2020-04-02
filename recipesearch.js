4501b1698eabb698738b354dce2c534ead4dd538
TusharK <ktusharhk@gmail.com>
2020-04-02 20:48:26 +1100

    resolve conflict

--- recipesearch.js
+++ recipesearch.js
@@ -5,9 +5,9 @@ var APIKey = "96aa363d6b8b4e798af15863b16aaf0e"
 var queryParams = getQueryParams();
 
 function getQueryParams() {
-     var queryParams = "";
+    var queryParams = "";
     for (var i = 0; i < ingredientArray.length; i++) {
-         if (i == 0) {
+        if (i == 0) {
             queryParams += ingredientArray[i];
         } else {
             queryParams += ",+" + ingredientArray[i];
@@ -26,42 +26,55 @@ function addNewIngredient() {
     $('#addBtn').on('click', function (event) {
         event.preventDefault();
         txtSearchIngredient = $("#searchIngredient").val()
-        if (txtSearchIngredient.length === 0 ) {
+        if (txtSearchIngredient.length === 0) {
             $("#searchIngredient").attr("placeholder", "Please type an ingredient.")
             return
         } else {
-        $("#searchIngredient").val("")
-        $("#searchIngredient").attr("placeholder", "Ingredient added to list!")
-        ingredientArray = txtSearchIngredient.split(",")
-        for (i = 0; i < ingredientArray.length; i++) {
-            var newFood = $('<button>')
-            newFood.text(ingredientArray[i].trim())
-            $("#selectFood").append(newFood)
-        }}
+            $("#searchIngredient").val("")
+            $("#searchIngredient").attr("placeholder", "Ingredient added to list!")
+            ingredientArray = txtSearchIngredient.split(",")
+            for (i = 0; i < ingredientArray.length; i++) {
+                var newFood = $('<button>')
+                newFood.text(ingredientArray[i].trim())
+                $("#selectFood").append(newFood)
+            }
+        }
     })
 }
 addNewIngredient()
 
 function searchIngredients() {
-    
+
     $('#searchBtn').on('click', function () {
         event.preventDefault();
-        var queryParams = getQueryParams();
-        
-        var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + queryParams + "&number=4" + "&apiKey=" + APIKey;
+        if (validateSearch()) {
+            var queryParams = getQueryParams();
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log("response: " + JSON.stringify(response));
-            localStorage.setItem("searchResults", JSON.stringify(response));
-            window.location.href = "./SearchResultPage.html";
-        });
+            var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + queryParams + "&number=4" + "&apiKey=" + APIKey;
 
-    })
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+                console.log("response: " + JSON.stringify(response));
+                localStorage.setItem("searchResults", JSON.stringify(response));
+                window.location.href = "./SearchResultPage.html";
+            });
+
+        }
+    }
+    )
 }
 searchIngredients()
+
+function validateSearch() {
+    var validateInput = true;
+    if (ingredientArray.length == 0) {
+        validateInput = false;
+        alert("Please enter the ingredients to search");
+    }
+    return validateInput;
+}
 
 
 
